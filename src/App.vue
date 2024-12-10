6e6f547aabc69a47f17e6ed5921d1fbd74c76fed
darcioSoares <darcio80@gmail.com>
2024-12-10 10:57:13 -0300

    init project and add tailwindcss style lib

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-blue-500 text-white p-4 text-center">
+    <h1 class="text-2xl font-bold text-orange-700">Hello TailwindCSS!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Adicione os caminhos dos seus arquivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
